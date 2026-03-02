6e4d945ad7d0ff9fc74535021a6289d37d276099
aiden <alehman26@basecampcodingacademy.org>
2026-03-02 11:30:54 -0600

    first commit

---
+++ src/main/java/org/example/springm2a1/SpringM2A1Application.java
@@ -0,0 +1,13 @@
+package org.example.springm2a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringM2A1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringM2A1Application.class, args);
+    }
+
+}
